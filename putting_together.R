910c44cd2751e6998304e455cebdb6eb689b839e
richardastantonjr <rich_stanton2003@yahoo.com>
2016-11-22 15:17:24 -0500

    calculated mass differences between extant and extinct species among continents and wrote the results to a csv. Not quite to direction yet. Need to tweak "spread function"
    Noticed "AF" and "Af" occur where "Af" appears to be a typo and changed all continent values toupper tocorrect.

--- putting_together.R
+++ putting_together.R
@@ -1,8 +1,25 @@
 
+library(tidyverse)
 
 mammal_sizes<-read.csv("./data/MOMv3.3.txt", stringsAsFactors = FALSE, na.strings = "-999",sep = "\t",header = FALSE)
 
 colnames(mammal_sizes) <- c("continent", "status", "order", 
                             "family", "genus", "species", "log_mass", "combined_mass", 
                             "reference")
+mammal_sizes$continent<-toupper(mammal_sizes$continent)
 
+## 2. Calculate the mean mass of the extinct species and the mean mass of the extant species. 
+select(mammal_sizes,status,log_mass) %>% 
+group_by(status) %>% 
+  summarise(mean(log_mass, na.rm=TRUE))
+
+## 3. Repeat the analysis but this time compare the mean masses within each of the different continents
+## Export your results to a csv file where the first entry on each line is the continent, the second entry 
+## is the average mass of the extant species on that continent, and the third entry is the average mass of 
+## the extinct species on that continent
+
+continent_mass_differences<-select(mammal_sizes,status,continent,log_mass) %>% 
+  group_by(continent,status) %>% 
+  summarise(mean_mass=mean(log_mass, na.rm=TRUE))
+continent_mass_differences<-spread(continent_mass_differences,continent,mean_mass)
+write.csv(continent_mass_differences,file = "./continent_mass_differences.csv")
